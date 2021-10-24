57984686b0575289acf9c42eff9c71346144b41d
zxd <meetzxd@gmail.com>
2021-10-24 21:32:44 +0800

    update

--- week3/lt106.cpp
+++ week3/lt106.cpp
@@ -15,7 +15,7 @@ public:
     vector<int> post;
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int in_len = inorder.size();
-        for(int i=0;i<in_len;++i){
+        for(int i=0 ; i < in_len; ++ i){
             in_pos[inorder[i]] = i;
         }
         
@@ -26,14 +26,14 @@ public:
     }
     
     TreeNode* buildTree(int in_left, int in_right, int post_left, int post_right){
-        if(in_left>in_right || post_left>post_right) return NULL;
+        if(in_left > in_right || post_left > post_right) return NULL;
         
         int root_val = post[post_right];
         int root_pos = in_pos[root_val];
         TreeNode* node = new TreeNode(root_val);
         
-        node->left = buildTree(in_left, root_pos-1, post_left, post_left+root_pos-in_left-1);
-        node->right = buildTree(root_pos+1, in_right, post_left+root_pos-in_left, post_right-1);
+        node->left = buildTree(in_left, root_pos - 1, post_left, post_left + root_pos - in_left - 1);
+        node->right = buildTree(root_pos + 1, in_right, post_left + root_pos - in_left, post_right - 1);
         
         return node;        
     }
